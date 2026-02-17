558b7d9e8239b18c168f56a9e1eb8f0ba6ce0c8e
mikaylassc <mikaylassc@gmail.com>
2026-02-17 11:42:45 -0500

    Add function to display user details
    Implement display_user() function with parameters

---
+++ display_user.py
@@ -0,0 +1,18 @@
+"""
+Mikayla Settles-Chambers
+CMSC 111
+Spring 2026
+Assignment 2 Week 4 (Functions)
+"""
+def display_user(name, age):
+    # This function prints the user's name and age
+    print("Name:", name)
+    print("Age:", age)
+
+display_user("Mikayla", 21)
+
+'''
+Expected Output: 
+Name: Mikayla
+Age: 21
+''' 
